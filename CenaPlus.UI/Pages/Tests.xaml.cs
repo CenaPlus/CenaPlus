42c8c42ce6508656cb7e7216f11823871c806e2e
Gasai Yuno <1@4321.io>
2014-01-24 12:16:34 +0800

    Contest List UI Demo

--- CenaPlus.UI/Pages/Tests.xaml.cs
+++ CenaPlus.UI/Pages/Tests.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CenaPlus.Entity;
 
 namespace CenaPlus.UI.Pages
 {
@@ -20,9 +21,33 @@ namespace CenaPlus.UI.Pages
     /// </summary>
     public partial class Tests : UserControl
     {
+        protected List<ContestList> ContestList = new List<ContestList>();
         public Tests()
         {
             InitializeComponent();
+            for (int i = 1; i <= 10; i++)
+            {
+                ContestList t = new ContestList();
+                t.Begin = Convert.ToDateTime("2014-1-24 12:00");
+                t.End = Convert.ToDateTime("2014-1-24 14:00");
+                t.Format = i%4;
+                t.Host = "Official";
+                t.Rating = 10;
+                t.Title = "Cena Plus Beta Round #" + i;
+                ContestList.Add(t);
+            }
+            ContestListBox.ItemsSource = ContestList;
+        }
+    }
+    public class ContestList : CenaPlus.Entity.Contests
+    {
+        private const string DetailTemplate = "{0} UTC / {1} hrs / {2} Format / Rating {3}";
+        public string Detail
+        {
+            get 
+            {
+                return String.Format(DetailTemplate, Begin, (Length / 60 / 60).ToString("0.0"), FormatStr[Format], Rating);
+            }
         }
     }
 }
