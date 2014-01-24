5cc1c680c7e6ac667fbfce4ec4a7de6f691342eb
Gasai Yuno <1@4321.io>
2014-01-24 21:16:42 +0800

    Records page

---
+++ CenaPlus.UI/Pages/Records.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CenaPlus.UI.Pages
+{
+    /// <summary>
+    /// Interaction logic for Records.xaml
+    /// </summary>
+    public partial class Records : UserControl
+    {
+        List<string> a = new List<string>();
+        public Records()
+        {
+            
+            InitializeComponent();
+            a.Add("");
+            a.Add("");
+            a.Add("");
+            a.Add("");
+            a.Add("");
+            a.Add("");
+            a.Add("");
+            a.Add("");
+            a.Add("");
+            RecordListBox.ItemsSource = a;
+        }
+    }
+}
