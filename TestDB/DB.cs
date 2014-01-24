9a3e4984ecd1ffe8e670c8f3579432f6b17ba6ff
onetwogoo <onetwogoo@live.com>
2014-01-24 22:13:39 +0800

    test ef

---
+++ TestDB/DB.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace TestDB
+{
+    enum ContestType
+    {
+        CF, OI
+    }
+    [Table("contests")]
+    class Contest
+    {
+        [Column("id")]
+        public long ID { get; set; }
+
+        [Column("type")]
+        public ContestType Type { get; set; }
+
+        [ForeignKey("ID")]
+        public virtual ICollection<Problem> Problems { get; set; }
+    }
+    [Table("problems")]
+    class Problem
+    {
+        [Column("id")]
+        public long ID { get; set; }
+
+        [Column("title")]
+        public string Title { get; set; }
+
+        [Column("content")]
+        public string Content { get; set; }
+
+        [Column("contest_id")]
+        [ForeignKey("Contest")]
+        public long ContestID { get; set; }
+
+        public Contest Contest { get; set; }
+    }
+    class DB : DbContext
+    {
+        public DbSet<Problem> Problems { get; set; }
+        public DbSet<Contest> Contests { get; set; }
+        public DB()
+            : base("mysqldb")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Problem>().HasRequired(s => s.Contest)
+           //.WithMany(s => s.Problems).HasForeignKey(s => s.ContestID);
+        }
+    }
+}
---
+++ TestDB/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new DB();
+            /*
+            Contest contest = new Contest
+            {
+                Type=ContestType.OI
+            };
+            db.Contests.Add(contest);
+            db.Problems.Add(new Problem
+            {
+                Title="Titlt",
+                Content="asdfasdfasdf",
+                Contest = contest
+            });
+            db.SaveChanges();
+             * */
+            Problem p = db.Problems.Where(x=>x.ID==1).Single();
+            Console.WriteLine(p.Contest);
+           
+        }
+    }
+}
