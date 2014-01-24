34db2f5aae64440de6a692722b76912aa7dd534f
Gasai Yuno <1@4321.io>
2014-01-24 17:44:23 +0800

    settings

--- CenaPlus.UI/Pages/Home.xaml.cs
+++ CenaPlus.UI/Pages/Home.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using FirstFloor.ModernUI.Windows.Navigation;
+using FirstFloor.ModernUI.Windows.Controls;
 
 namespace CenaPlus.UI.Pages
 {
@@ -20,14 +22,44 @@ namespace CenaPlus.UI.Pages
     /// </summary>
     public partial class Home : UserControl
     {
+        public static bool Logged=true;
         public Home()
         {
             InitializeComponent();
+            if (Logged)
+            {
+                var frame = NavigationHelper.FindFrame(null, this);
+                if (frame != null)
+                {
+                    frame.Source = new Uri("/Content/Compiler.xaml", UriKind.Relative);
+                }
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            NavigationService.GetNavigationService(this).Navigate(new Uri("/Settings.xaml", UriKind.Relative));
+            ModernDialog.ShowMessage("Cannot connect to the target server.", "Message", System.Windows.MessageBoxButton.OK);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var frame = NavigationHelper.FindFrame(null, this);
+            if (frame != null)
+            {
+                frame.Source = new Uri("/Content/Compiler.xaml", UriKind.Relative);
+            }
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (Logged)
+            {
+                var frame = NavigationHelper.FindFrame(null, this);
+                if (frame != null)
+                {
+                    //frame.Source = new Uri("/Pages/Main.xaml", UriKind.Relative);
+                }
+            }
         }
     }
 }
