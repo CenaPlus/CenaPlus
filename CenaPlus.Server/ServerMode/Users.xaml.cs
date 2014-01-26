2a8bf42a04f19cd9db320a4e60fa90e945a46cab
Gasai Yuno <1@4321.io>
2014-01-27 00:03:18 +0800

    Server users manage page

--- CenaPlus.Server/ServerMode/Users.xaml.cs
+++ CenaPlus.Server/ServerMode/Users.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Security;
+using System.Security.Cryptography;
+using CenaPlus.Entity;
 
 namespace CenaPlus.Server.ServerMode
 {
@@ -20,9 +23,51 @@ namespace CenaPlus.Server.ServerMode
     /// </summary>
     public partial class Users : UserControl
     {
+        public List<UserListItem> UserList = new List<UserListItem>();
         public Users()
         {
             InitializeComponent();
+            for (int i = 1; i <= 10; i++)
+            {
+                UserListItem t = new UserListItem();
+                t.ID = i;
+                t.Name = "shabi#"+i;
+                t.Role = UserRole.Competitor;
+                UserList.Add(t);
+            }
+            UserListBox.ItemsSource = UserList;
+        }
+
+        private void UserListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (UserListBox.SelectedItem != null)
+            {
+                ProfileDisplay.Visibility = Visibility.Visible;
+                NameTextBox.Text = (UserListBox.SelectedItem as UserListItem).Name;
+            }
+            else
+            {
+                ProfileDisplay.Visibility = Visibility.Hidden;
+            }
+        }
+    }
+
+    public class UserListItem : Entity.User
+    {
+        public string Gravatar
+        {
+            get 
+            {
+                return @"https://www.gravatar.com/avatar/159c4a0a78d0980aca8df9d781d1c755?d=https://www.SmartOJ.com/img/Non_Avatar.png&s=50";
+                //return System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(Gravatar, "MD5").ToLower();
+            }
+        }
+        public string Profile
+        {
+            get 
+            {
+                return String.Format("{0} / Email: {1}", Role.ToString(), "???");
+            }
         }
     }
 }
