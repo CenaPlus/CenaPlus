8b44c5cf4e61e9191777361fe11b5f0a0877b53c
onetwogoo <onetwogoo@live.com>
2014-01-26 14:25:35 +0800

    integrate embedded mysql

--- CenaPlus.Server/App.xaml.cs
+++ CenaPlus.Server/App.xaml.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Reflection;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
-
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
 namespace CenaPlus.Server
 {
     /// <summary>
@@ -13,5 +17,53 @@ namespace CenaPlus.Server
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            string appPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string mySqlPath = Path.Combine(appPath, "mysql");
+
+            bool mySqlRunning = false;
+            if (File.Exists(Path.Combine(mySqlPath, "data\\mysqld.pid")))
+            {
+                mySqlRunning = true;
+                // Try to connect mysql
+                try
+                {
+                    Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    sock.Connect(new IPEndPoint(IPAddress.Loopback, 3311));
+                    sock.Close();
+                }
+                catch
+                {
+                    mySqlRunning = false;
+                    File.Delete(Path.Combine(mySqlPath, "data\\mysqld.pid"));
+                }
+            }
+
+            if (!mySqlRunning)
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    CreateNoWindow = true,
+                    FileName = Path.Combine(mySqlPath, "bin\\mysqld.exe"),
+                    WorkingDirectory = mySqlPath,
+                    WindowStyle = ProcessWindowStyle.Hidden
+                });
+            }
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            string appPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string mySqlPath = Path.Combine(appPath, "mysql");
+            Process.Start(new ProcessStartInfo
+            {
+                Arguments="--user=root --port=3311 shutdown",
+                CreateNoWindow = true,
+                FileName = Path.Combine(mySqlPath, "bin\\mysqladmin.exe"),
+                WorkingDirectory = mySqlPath,
+                WindowStyle = ProcessWindowStyle.Hidden
+            });
+        }
     }
 }
