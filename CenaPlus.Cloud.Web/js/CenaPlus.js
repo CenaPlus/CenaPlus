80e4d452930959ce50c2a8a9275ffa9a50dda01f
Gasai Yuno <1@4321.io>
2014-02-12 20:29:48 +0800

    Cena+ Cloud Web

---
+++ CenaPlus.Cloud.Web/js/CenaPlus.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > $('#header-featured').height() + $('#header-wrapper').height() + 312) {
+            if (!$("#header-wrapper").hasClass("LightHeader")) {
+                $("#header-wrapper").addClass("LightHeader");
+                $("#menu").removeClass("Menu-Color");
+            }
+        }
+        else {
+            if ($("#header-wrapper").hasClass("LightHeader")) {
+                $("#header-wrapper").removeClass("LightHeader");
+                $("#header-wrapper").hide();
+                $("#header-wrapper").fadeIn(250);
+                $("#menu").addClass("Menu-Color");
+            }
+        }
+    });
+});
