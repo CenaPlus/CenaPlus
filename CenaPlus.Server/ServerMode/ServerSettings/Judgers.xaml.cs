bb9570a8b9432ddd3664caf2973a9e83899d2ff0
Gasai Yuno <1@4321.io>
2014-02-14 19:58:51 +0800

    server settings judge node

--- CenaPlus.Server/ServerMode/ServerSettings/Judgers.xaml.cs
+++ CenaPlus.Server/ServerMode/ServerSettings/Judgers.xaml.cs
@@ -20,9 +20,41 @@ namespace CenaPlus.Server.ServerMode.ServerSettings
     /// </summary>
     public partial class Judgers : UserControl
     {
+        public List<JudgeNodeListBoxItem> JudgeNodeListBoxItems = new List<JudgeNodeListBoxItem>();
         public Judgers()
         {
             InitializeComponent();
+            for (int i = 1; i <= 10; i++)
+            {
+                JudgeNodeListBoxItem t = new JudgeNodeListBoxItem();
+                t.Connected = Convert.ToBoolean(i % 2);
+                t.Name = "Cena+ Judge Node #" + (i - 1);
+                t.IP = "192.168.0." + i;
+                JudgeNodeListBoxItems.Add(t);
+            }
+            JudgeNodeListBox.ItemsSource = JudgeNodeListBoxItems;
+        }
+
+        private void JudgeNodeListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (JudgeNodeListBox.SelectedItem != null)
+            {
+                btnConnect.IsEnabled = !(JudgeNodeListBox.SelectedItem as JudgeNodeListBoxItem).Connected;
+                btnDisconnect.IsEnabled = (JudgeNodeListBox.SelectedItem as JudgeNodeListBoxItem).Connected;
+            }
+        }
+    }
+    public class JudgeNodeListBoxItem
+    {
+        public string IP { get; set; }
+        public string Name { get; set; }
+        public bool Connected { get; set; }
+        public string Details
+        {
+            get 
+            {
+                return String.Format("{0}{1}", Connected ? "Connected " : "", IP);
+            }
         }
     }
 }
